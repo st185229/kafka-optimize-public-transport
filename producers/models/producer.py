0f09e7118a2d5c1819770de2b816a05f40da9fb6
st185229 <suresh.thomas.uk@gmail.com>
2021-05-19 23:41:34 +0100

    Create Kafka Producers

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -1,13 +1,46 @@
-"""Producer base-class providing common utilites and functionality"""
+"""Producer base-class providing common utilities and functionality"""
 import logging
 import time
 
-
-from confluent_kafka import avro
 from confluent_kafka.admin import AdminClient, NewTopic
-from confluent_kafka.avro import AvroProducer
 
 logger = logging.getLogger(__name__)
+
+
+def topic_exists(client, topic_name):
+    """Checks if the given topic exists"""
+    topic_metadata = client.list_topics(timeout=5)
+    return topic_name in set(t.topic for t in iter(topic_metadata.topics.values()))
+
+
+def create_topic(client, topic_name):
+    """Creates the topic with the given topic name"""
+    futures = client.create_topics(
+        [
+            NewTopic(
+                topic=topic_name,
+                num_partitions=10,
+                replication_factor=1,
+                config={
+                    "cleanup.policy": "delete",
+                    "compression.type": "lz4",
+                    "delete.retention.ms": "2000",
+                    "file.delete.delay.ms": "2000",
+                },
+            )
+        ]
+    )
+
+    for topic, future in futures.items():
+        try:
+            future.result()
+            print("topic created")
+        except Exception as e:
+            print(f"failed to create topic {topic_name}: {e}")
+
+
+def time_millis():
+    return int(round(time.time() * 1000))
 
 
 class Producer:
@@ -17,12 +50,12 @@ class Producer:
     existing_topics = set([])
 
     def __init__(
-        self,
-        topic_name,
-        key_schema,
-        value_schema=None,
-        num_partitions=1,
-        num_replicas=1,
+            self,
+            topic_name,
+            key_schema,
+            value_schema=None,
+            num_partitions=1,
+            num_replicas=1,
     ):
         """Initializes a Producer object with basic settings"""
         self.topic_name = topic_name
@@ -38,6 +71,7 @@ class Producer:
         #
         #
         self.broker_properties = {
+            "bootstrap.servers": "PLAINTEXT://localhost:9092"
             # TODO
             # TODO
             # TODO
@@ -54,26 +88,31 @@ class Producer:
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
+        client = AdminClient(self.broker_properties)
+        exists = topic_exists(client, self.topic_name)
+        logger.info(f"Topic {self.topic_name} exists: {exists}")
+        if exists is False:
+            create_topic(client, self.topic_name)
+        else:
+            logger.info(f"Topic {self.topic_name} already exists")
         #
         #
         # TODO: Write code that creates the topic for this producer if it does not already exist on
         # the Kafka Broker.
         #
         #
-        logger.info("topic creation kafka integration incomplete - skipping")
-
-    def time_millis(self):
-        return int(round(time.time() * 1000))
 
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
+        logger.info("producer close ")
+        Producer.existing_topics.discard(self.topic_name)
         #
         #
         # TODO: Write cleanup code for the Producer here
         #
         #
-        logger.info("producer close incomplete - skipping")
 
+    @property
     def time_millis(self):
         """Use this function to get the key for Kafka Events"""
         return int(round(time.time() * 1000))
--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -7,7 +7,6 @@ from confluent_kafka import avro
 from models.producer import Producer
 from models.turnstile_hardware import TurnstileHardware
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -17,18 +16,18 @@ class Turnstile(Producer):
     #
     # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
     #
-    #value_schema = avro.load(
-    #    f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
-    #)
+    value_schema = avro.load(
+        f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+    )
 
     def __init__(self, station):
         """Create the Turnstile"""
         station_name = (
             station.name.lower()
-            .replace("/", "_and_")
-            .replace(" ", "_")
-            .replace("-", "_")
-            .replace("'", "")
+                .replace("/", "_and_")
+                .replace(" ", "_")
+                .replace("-", "_")
+                .replace("'", "")
         )
 
         #
@@ -38,22 +37,32 @@ class Turnstile(Producer):
         #
         #
         super().__init__(
-            f"{station_name}", # TODO: Come up with a better topic name
+            f"{station_name}",  # TODO: Come up with a better topic name
+            topic_name=self.topic_name,
             key_schema=Turnstile.key_schema,
-            # TODO: value_schema=Turnstile.value_schema, TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Turnstile.value_schema,
+            num_replicas=1,
+            num_partitions=5,
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
+        self.topic_name = "com.udacity.station.turnstile"
 
     def run(self, timestamp, time_step):
         """Simulates riders entering through the turnstile."""
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
-        logger.info("turnstile kafka integration incomplete - skipping")
-        #
-        #
-        # TODO: Complete this function by emitting a message to the turnstile topic for the number
-        # of entries that were calculated
-        #
-        #
+        logger.info("sending message for each")
+        for i in range(num_entries):
+            Producer.produce(self,
+                             topic=self.topic_name,
+                             key={"timestamp": self.time_millis()},
+                             value={
+                                 "station_id": self.station.station_id,
+                                 "station_name": self.station.name,
+                                 "line": self.station.color,
+                             },
+                             )
+
+
+if __name__ == "__main__":
+    Turnstile().run()
